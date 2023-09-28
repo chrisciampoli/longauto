39e5204507a32dde9c46a046903bc9c69a270a4e
Christopher Ciampoli <chris@robotcache.com>
2023-09-28 14:00:24 -0400

    Adding config

---
+++ app/src/config/config.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'db' => [
+        'host' => 'localhost',
+        'dbname' => 'mydb',
+        'user' => 'username',
+        'password' => 'password'
+    ],
+    // ... other configurations
+];
